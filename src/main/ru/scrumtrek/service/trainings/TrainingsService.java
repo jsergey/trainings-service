3b32cfe2a25929f13fb33e8e1b58bacef575ffe1
Sergey Baranov <jsergey@gmail.com>
2017-03-12 23:33:19 +0300

    back to defaults

--- src/main/ru/scrumtrek/service/trainings/TrainingsService.java
+++ src/main/ru/scrumtrek/service/trainings/TrainingsService.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
-/**
- * Created by sergeybaranov on 12/3/17.
- */
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @Import(ServiceConfiguration.class)
@@ -19,7 +16,6 @@ public class TrainingsService {
 
     public static void main(String[] args) {
 
-        System.setProperty("spring.config.name", "trainings-service");
         SpringApplication.run(TrainingsService.class, args);
 
 
