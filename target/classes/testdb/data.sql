8dfb61d6b5b47e5244cc2b82e22927a71bdb7ae7
Sergey Baranov <jsergey@gmail.com>
2017-03-12 23:42:42 +0300

    remove target dir

--- target/classes/testdb/data.sql
+++
@@ -1,2 +0,0 @@
-insert into Training (training_name, training_description) values ('CAP', 'cert icagile training');
-insert into Training (training_name, training_description) values ('KANBAN', 'kanban system');
--- target/classes/testdb/schema.sql
+++
@@ -1,2 +0,0 @@
-
-create table Training (ID bigint identity primary key, training_name varchar(100) not null, training_description varchar(500) not null);
